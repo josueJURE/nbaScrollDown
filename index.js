6410cf960f4d4d121b7376bead3e0d27119debbc
josueJURE <josue.jure@gmail.com>
2022-01-12 17:16:48 +0000

    use background-image: url() on ul element

--- index.js
+++ index.js
@@ -4,13 +4,17 @@ console.log(pictures)
 
 for(var i = 0; i < pictures.length; i++) {
   pictures[i].addEventListener("mouseover", function() {
-    this.lastElementChild.style.transform = "translateX(30%)";
+    this.lastElementChild.style.transform = "translateX(0%)";
     this.lastElementChild.style.opacity = 1;
+    this.lastElementChild.style.transition = "all 4s"
+    this.lastElementChild.style.transition.timing.function = "ease-in-out";
   })
 }
 
 for(var i = 0; i < pictures.length; i++) {
   pictures[i].addEventListener("mouseleave", function() {
-    this.lastElementChild.style.display = "none";
+    this.lastElementChild.style.transform = "translateX(100%)";
+    this.lastElementChild.style.transition = 0;
+    this.lastElementChild.style.opacity = 0;
   })
 }
